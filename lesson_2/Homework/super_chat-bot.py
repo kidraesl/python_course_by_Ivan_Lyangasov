cfdf90bf49d065f187b907993ed5a085d22a9753
kidraesl <107318839+kidraesl@users.noreply.github.com>
2022-06-19 22:07:46 +0300

    комиит для дебага

--- lesson_2/Homework/super_chat-bot.py
+++ lesson_2/Homework/super_chat-bot.py
@@ -1,21 +1,25 @@
-user_text = input('введите фразу ')
-greeting_phrases = ['привет', 'дароу', 'добрый день']
-greeting_phrases_2 = ['до свидания', 'пока', 'покеда', 'доброго вечера']
-greeting_phrases_3 = ['бля', 'черт', 'блин']
+met_bye_phrase = False
+while (met_bye_phrase == False):
+    user_text = input('введите фразу ')
+    greeting_phrases = ['привет', 'дароу', 'добрый день']
+    goodbye_phrases = ['до свидания', 'пока', 'покеда', 'доброго вечера']
+    bad_phrases = ['бля', 'черт', 'блин']
 
-for greeting_frase in greeting_phrases:
-    if user_text == greeting_frase:
-        print('привет и тебе!')
-        break
-else:
-    for greeting_frase in greeting_phrases_2:
-        if user_text == greeting_frase:
-            print('до свидания!')
+    for greeting_frase in greeting_phrases:
+        if greeting_frase in user_text:
+            print('привет и тебе!')
             break
     else:
-        for greeting_frase in greeting_phrases_3:
-            if user_text == greeting_frase:
-                print('не ругайся')
+        for goodbye_frase in goodbye_phrases:
+            if goodbye_frase in user_text:
+                print('до свидания!')
+                met_bye_phrase = True
                 break
-            else:
-                print('я тебя не понимаю')
+        else:
+            for bad_frase in bad_phrases:
+                if bad_frase in user_text:
+                    print('не ругайся')
+                    break
+                else:
+                    print('я тебя не понимаю')
+                    break

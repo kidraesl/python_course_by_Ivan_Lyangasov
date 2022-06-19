79802c3769fe9762452f4db1aa3a67cf13809709
kidraesl <107318839+kidraesl@users.noreply.github.com>
2022-06-19 21:06:56 +0300

    супер_чат-бот(дз2) - убрал пробелы

--- lesson_2/Homework/super_chat-bot.py
+++ lesson_2/Homework/super_chat-bot.py
@@ -4,24 +4,18 @@ greeting_phrases_2 = ['до свидания', 'пока', 'покеда', 'до
 greeting_phrases_3 = ['бля', 'черт', 'блин']
 
 for greeting_frase in greeting_phrases:
-
     if user_text == greeting_frase:
         print('привет и тебе!')
         break
-
 else:
     for greeting_frase in greeting_phrases_2:
-
         if user_text == greeting_frase:
             print('пока!')
             break
-
     else:
         for greeting_frase in greeting_phrases_3:
-
             if user_text == greeting_frase:
                 print('не ругайся')
                 break
-
             else:
                 print('я тебя не понимаю')
